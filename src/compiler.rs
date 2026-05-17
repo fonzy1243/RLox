05dda288caa9305eecc6bc61efdceeb7b245b181
Fonzy Fermin <fonzy.fermin@gmail.com>
2026-05-17 21:24:56 +0800

    feat: add test output for tokens

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,10 +1,27 @@
-use crate::scanner::Scanner;
+use crate::scanner::{Scanner, TokenType};
 
 pub fn compile(source: &str) {
     let mut scanner = Scanner::new(source);
-    let mut line = usize::MAX;
+    let mut line: i64 = -1;
 
     loop {
         let token = scanner.scan_token();
+
+        if token.line as i64 != line {
+            print!("{:4} ", token.line);
+            line = token.line as i64;
+        } else {
+            print!("   | ");
+        }
+
+        println!(
+            "{:2} '{}'",
+            token.token_type as i32,
+            &token.start[..token.length]
+        );
+
+        if token.token_type == TokenType::Eof {
+            break;
+        }
     }
 }
